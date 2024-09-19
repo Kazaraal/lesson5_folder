737eb38ef1567e4d84b30c7990ee7f79a844c0f7
ken0 <araalkaz@gmail.com>
2024-09-19 21:07:28 +0100

    The rest of the lesson 1 homework.

--- lesson_1.py
+++ lesson_1.py
@@ -21,3 +21,59 @@
 # my_number = str(5+5) 
 # print("result is: "+my_number)
 
+# D. 
+# What will be the output?  
+ 
+# x = 5  
+# y = 2.36  
+# print (x+int (y) )
+
+# This prints 7
+
+# E. 
+# 1. Create two variables named X and Y. 
+# 2. Print “BIG” if X is bigger than Y. 
+# 3. Print “small” if X is smaller than Y. 
+
+# x = input("Please provide a first number. Let's call it X: ")
+# y = input("Please provide a second number. Let's call it Y: ")
+
+# if x > y:
+#     print("BIG")
+# else:
+#     print("small")
+
+
+# F. 
+# 1. Create a variable and initialize it with a number 1-4. 
+# 2. Create 4 conditions (if-elif) which will check the variable. 
+# 3. Print the season name accordingly: 
+ 
+# - 1 = summer 
+# - 2 = winter 
+# - 3 = fall 
+# - 4 = spring 
+ 
+# lst = [1, 2, 3, 4]
+# season = int(input("Please provide a number between 1-4: "))
+# if season == lst[0]:
+#     print("Summer")
+# elif season == lst[1]:
+#     print("Winter")
+# elif season == lst[2]:
+#     print("Fall")
+# elif season == lst[3]:
+#     print("Spring")
+# else:
+#     print("Please provide a valid number")
+
+
+
+# CHALLENGE: 
+# Fix the following code without changing a or b:  
+ 
+# a = 8  
+# b = "123" 
+# b = int("123") 
+ 
+# print(a+b) 
