da68c6bb64deb8d57669f74659f1aa07a5d630f8
ken0 <araalkaz@gmail.com>
2024-09-19 20:33:56 +0100

    Added answers B and C

--- lesson_1.py
+++ lesson_1.py
@@ -5,3 +5,19 @@
 # print(first * second)
 # print(second / first)
 
+# B:
+# a = 9
+# b = 8
+# c = 15
+
+# C:
+# name = “john”     
+# name= 'john'
+# As long as you use "" or '', what is printed will be similar. 
+
+
+# 2. What is the issue with the code below? 
+ 
+# my_number = str(5+5) 
+# print("result is: "+my_number)
+
