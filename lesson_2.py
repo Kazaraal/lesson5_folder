d168df81f244c76d55e0ad1de97973e29a30d278
ken0 <araalkaz@gmail.com>
2024-09-20 08:55:50 +0100

    Solution until F

--- lesson_2.py
+++ lesson_2.py
@@ -19,3 +19,29 @@
 #     print(season[number_by_user])
 # else:
 #     print("Invalid number")
+
+# D:
+# count = 1
+# while count < 11:
+#     print(count)
+#     count += 1
+
+# 1. 10 times
+# 2. 10
+
+# E:
+# age = int(input("Enter your age: "))
+# first_letter = input("Enter the first letter of your last name: ")
+# current_shekels_dollar_exchange_rate = float(input("Enter the current shekels-dollar exchange rate: "))
+# fly_abroad = input("True/False: Do you want to fly abroad? ")
+# apartment_number = int(input("Enter your apartment number: "))
+
+# print(f"Age: {age} \nFirst letter of your last name: {first_letter} \nCurrent shekels-dollar currency: {current_shekels_dollar_exchange_rate} \nYou flew abroad: {fly_abroad} \nAnd your apartment number is: {apartment_number}")
+
+# print(age + current_shekels_dollar_exchange_rate)
+
+# F:
+# phone_number = input("Enter your phone number: ")
+# print("Phone number: " + phone_number)
+
+# G:
