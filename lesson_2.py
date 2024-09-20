b0988343cd5d1e4619c35ba69dfa249a49aa3a90
ken0 <araalkaz@gmail.com>
2024-09-20 08:27:39 +0100

    Solution for section A, B and C of lesson 2.

---
+++ lesson_2.py
@@ -0,0 +1,21 @@
+# A:
+# x = int(input("Enter the first number: "))
+# y = int(input("Enter the second number: "))
+
+# if x > y:
+#     print("BIG")
+# else:
+#     print("small")
+
+
+# B:
+# for number in range(1, 6):
+#     print(number)
+
+# C:
+# season = {1: "summer", 2: "winter", 3: "fall", 4: "spring"}
+# number_by_user = int(input("Enter a number between 1-4: "))
+# if number_by_user in season:
+#     print(season[number_by_user])
+# else:
+#     print("Invalid number")
