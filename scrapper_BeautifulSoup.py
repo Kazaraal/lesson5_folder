a83531c2f601e501d2d3099b07f5d4e49cd58824
kazaraal <araalkaz@gmail.com>
2024-09-29 20:44:03 +0100

    Made it a function and added the ability to create a csv of the results.

--- scrapper_BeautifulSoup.py
+++ scrapper_BeautifulSoup.py
@@ -10,7 +10,10 @@
 # Install requests (pip install requests)
 # Import BeautifulSoup
 from bs4 import BeautifulSoup
+# Import requests
 import requests
+# Import csv
+import csv
 
 def scrapper_BeautifulSoup():
     website_to_scrape = "https://quotes.toscrape.com/"
@@ -19,7 +22,14 @@ def scrapper_BeautifulSoup():
     qoutes = soup.find_all("span", attrs={"class":"text"})
     authors = soup.find_all("small", attrs={"class","author"})
     
+    file = open("scrapped_quotes.csv", "w")
+    writer = csv.writer(file)
+
+    writer.writerow(["QUOTES", "AUTHORS"])
+
     for quote, author in zip(qoutes, authors):
         print(quote.text + "." + author.text)
+        writer.writerow([quote.text, author.text])
+    file.close()
 
 scrapper_BeautifulSoup()
